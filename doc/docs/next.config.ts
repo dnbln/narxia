7b8f6c3554136815163a60a3f77bca1e35046383
Dinu Blanovschi <git@dnbln.dev>
2025-05-25 12:37:41 +0200

    fix: minor docs patch

--- doc/docs/next.config.ts
+++ doc/docs/next.config.ts
@@ -1,18 +1,18 @@
 import nextra from 'nextra'
 
-import {remarkCodeHike, recmaCodeHike, CodeHikeConfig} from "codehike/mdx"
-import {PluggableList} from "unified";
+import { remarkCodeHike, recmaCodeHike, CodeHikeConfig } from "codehike/mdx"
+import { PluggableList } from "unified";
 
 const chConfig: CodeHikeConfig = {
     // optional (see code docs):
-    components: {code: "Code"},
+    components: { code: "Code" },
     // if you can't use RSC:
     syntaxHighlighting: {
-      theme: "github-dark",
+        theme: "github-dark",
     },
 };
 
-const mdxOptions: {remarkPlugins: PluggableList, recmaPlugins: PluggableList} = {
+const mdxOptions: { remarkPlugins: PluggableList, recmaPlugins: PluggableList } = {
     remarkPlugins: [[remarkCodeHike, chConfig]],
     recmaPlugins: [[recmaCodeHike, chConfig]],
 }
@@ -31,4 +31,29 @@ const withNextra = nextra({
 export default withNextra({
     // ... Other Next.js config options
     output: 'export',
+    webpack(config) {
+        // Grab the existing rule that handles SVG imports
+        const fileLoaderRule = config.module.rules.find((rule: any) => rule.test?.test?.('.svg'))
+
+        config.module.rules.push(
+            // Reapply the existing rule, but only for svg imports ending in ?url
+            {
+                ...fileLoaderRule,
+                test: /\.svg$/i,
+                resourceQuery: /url/, // *.svg?url
+            },
+            // Convert all other *.svg imports to React components
+            {
+                test: /\.svg$/i,
+                issuer: fileLoaderRule.issuer,
+                resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+                use: ['@svgr/webpack'],
+            },
+        )
+
+        // Modify the file loader rule to ignore *.svg, since we have it handled now.
+        fileLoaderRule.exclude = /\.svg$/i
+
+        return config
+    },
 })
--- doc/docs/src/app/layout.tsx
+++ doc/docs/src/app/layout.tsx
@@ -1,12 +1,14 @@
 import { Footer, Layout, Navbar } from 'nextra-theme-docs'
 import {
     // Banner,
-    Head } from 'nextra/components'
+    Head
+} from 'nextra/components'
 import { getPageMap } from 'nextra/page-map'
 import 'nextra-theme-docs/style.css'
-import {ReactNode} from "react";
+import { ReactNode } from "react";
 import './globals.css'
 import narxiaLogo from '../../../logo/narxia.png'
+import ZulipLogo from './zulip-icon.svg'
 
 export const metadata = {
     // Define your metadata here
@@ -16,15 +18,16 @@ export const metadata = {
 // const banner = <Banner storageKey="some-key">Nextra 4.0 is released 🎉</Banner>
 const navbar = (
     <Navbar
-        logo={<b className={'flex flex-row justify-center align-middle'} style={{alignItems: 'center'}}><img src={narxiaLogo.src} height='64px' width='64px' alt='narxia logo'/> The Narxia Programming Language</b>}
+        logo={<b className={'flex flex-row justify-center align-middle'} style={{ alignItems: 'center' }}><img src={narxiaLogo.src} height='64px' width='64px' alt='narxia logo' /> The Narxia Programming Language</b>}
         projectLink={'https://github.com/dnbln/narxia'}
-        chatLink={'https://discord.gg/hello'}
-        // ... Your additional navbar options
+        chatLink={'https://nrx.zulipchat.com'}
+        chatIcon={<ZulipLogo width='32px' height='32px'/>}
+    // ... Your additional navbar options
     />
 )
-const footer = <Footer>Copyright {new Date().getFullYear()} © Dinu Blanovschi.<br/>Licensed under CC 4.0 BY-SA-NC</Footer>
+const footer = <Footer>Copyright {new Date().getFullYear()} © Dinu Blanovschi.<br />Licensed under CC 4.0 BY-SA-NC</Footer>
 
-export default async function RootLayout({ children }: {children: ReactNode}) {
+export default async function RootLayout({ children }: { children: ReactNode }) {
     return (
         <html
             // Not required, but good for SEO
@@ -34,24 +37,27 @@ export default async function RootLayout({ children }: {children: ReactNode}) {
             // Suggested by `next-themes` package https://github.com/pacocoursey/next-themes#with-app
             suppressHydrationWarning
         >
-        <Head
+            <Head
             // ... Your additional head options
 
-        >
-            {/* Your additional tags should be passed as `children` of `<Head>` element */}
-        </Head>
-        <body>
-        <Layout
-            // banner={banner}
-            navbar={navbar}
-            pageMap={await getPageMap()}
-            docsRepositoryBase="https://github.com/dnbln/narxia/edit/trunk/doc/docs"
-            footer={footer}
-            // ... Your additional layout options
-        >
-            {children}
-        </Layout>
-        </body>
+            >
+                {/* Your additional tags should be passed as `children` of `<Head>` element */}
+            </Head>
+            <body>
+                <Layout
+                    // banner={banner}
+                    navbar={navbar}
+                    pageMap={await getPageMap()}
+                    docsRepositoryBase="https://github.com/dnbln/narxia/edit/trunk/doc/docs"
+                    footer={footer}
+                    feedback={{
+                        link: 'https://nrx.zulipchat.com/'
+                    }}
+                // ... Your additional layout options
+                >
+                    {children}
+                </Layout>
+            </body>
         </html>
     )
 }
