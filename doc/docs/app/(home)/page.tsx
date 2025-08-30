5f7b6d3864f3eaf376ad6099494a4c57a9c7e408
Dinu Blanovschi <git@dnbln.dev>
2025-08-30 22:49:59 +0200

    chore: doc home

--- doc/docs/app/(home)/page.tsx
+++ doc/docs/app/(home)/page.tsx
@@ -3,17 +3,18 @@ import Link from 'next/link';
 export default function HomePage() {
   return (
     <main className="flex flex-1 flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
-      <p className="text-fd-muted-foreground">
-        You can open the{' '}
-        <Link
-          href="/docs/user/user-intro"
-          className="text-fd-foreground font-semibold underline"
-        >
-          User documentation
-        </Link>{' '}
-        and see the documentation.
-      </p>
+      <div className='flex flex-col items-center'>
+        <h1 className="mb-4 text-2xl font-bold">Welcome to the Narxia programming language.</h1>
+        <p className="text-fd-muted-foreground">
+          Go to the{' '}
+          <Link
+            href="/docs/user/user-intro"
+            className="text-fd-foreground font-semibold underline"
+          >
+            User documentation
+          </Link>.
+        </p>
+      </div>
     </main>
   );
 }
