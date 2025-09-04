8478f0788614315613f69022ba30d04b0834cfdc
Dinu Blanovschi <git@dnbln.dev>
2025-09-04 09:50:23 +0200

    fix: docs build

--- doc/docs/app/(home)/layout.tsx
+++ doc/docs/app/(home)/layout.tsx
@@ -2,7 +2,6 @@ import type { ReactNode } from 'react';
 import { HomeLayout, HomeLayoutProps } from 'fumadocs-ui/layouts/home';
 import { baseOptions } from '@/app/layout.config';
 import { NarxiaIcon } from "@/lib/assets";
-import Link from 'fumadocs-core/link';
 
 const layoutProps: HomeLayoutProps = {
   ...baseOptions,
