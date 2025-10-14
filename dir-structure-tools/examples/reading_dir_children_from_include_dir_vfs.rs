c268422bd4aeabd49d5d245593b2cc3def481a2c
Dinu Blanovschi <git@dnbln.dev>
2025-10-14 14:07:34 +0200

    chore: fix

--- dir-structure-tools/examples/reading_dir_children_from_include_dir_vfs.rs
+++ dir-structure-tools/examples/reading_dir_children_from_include_dir_vfs.rs
@@ -17,7 +17,11 @@ pub struct Dir<Vfs: VfsCore> {
 }
 
 pub fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let vfs = dir_structure::include_dir_vfs!("$CARGO_MANIFEST_DIR/examples/example_dirs/children");
+    let vfs = dir_structure_include_dir_vfs::IncludeDirVfs::new(
+        dir_structure_include_dir_vfs::include_dir::include_dir!(
+            "$CARGO_MANIFEST_DIR/examples/example_dirs/children"
+        ),
+    );
 
     let dir = Dir::read_from(Path::new("."), Pin::new(&vfs))?;
 
